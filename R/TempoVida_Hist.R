b2d81c3c9d464f2d3e5d4b5e3a87d408f196b668
Caio Vieira <caiovr@gmail.com>
2018-07-04 17:15:07 -0300

    add a function that produces Tempo de Vida histogram

---
+++ R/TempoVida_Hist.R
@@ -0,0 +1,5 @@
+TempoVida_Hist <- function(c){
+  ggplot(ApolicesAuto %>% filter(MesCriacao > "Dec2016", !is.na(DataCancelamento), MotivoCancelamento %in% c), aes(x=TempoDeVida, fill=CS)) +
+    geom_histogram(aes(y =..density..),position="identity",  breaks=seq(0, 200, by=10),alpha = .3) +
+    theme_few() + theme(legend.position = "bottom") + labs(title = "Tempo de Vida das Apólices Canceladas")
+}
