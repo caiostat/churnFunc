419bf1a26406795120c38ff3eda5c4b95e48aa7a
Caio Vieira <caiovr@gmail.com>
2018-08-01 11:08:36 -0300

    Filters turned into mutates, so numbers reflect reallity

--- R/CohortPercent.R
+++ R/CohortPercent.R
@@ -11,17 +11,17 @@
 #' @export CohortPercent
 
 CohortPercent <- function(df, c, var, cols, cols2){
-  x <- df %>% mutate(ChurnVoluntario = ifelse(MotivoCancelamento == "Voluntário", 1, 0),
-                     ChurnInadimplencia = ifelse(MotivoCancelamento == "Inadimplência", 1, 0))  %>%
+  x <- df %>% mutate(ChurnAlvo = ifelse(MotivoCancelamento %in% c, "Sim", "Não"))  %>%
     mutate(MesCriacao = as.yearmon(DataCriacao)) %>%
     group_by(!!!as_quosure(cols)) %>% dplyr::summarise(n=n()) %>%
     group_by(!!!as_quosure(cols2)) %>% mutate(Total = sum(n),Percentual = round((n/Total)*100,1))
-  ggplot(x, aes(y=Percentual, x=MesCriacao, fill = CoreChurn)) +
+
+  ggplot(x, aes(y=Percentual, x=MesCriacao, fill = ChurnAlvo)) +
     geom_col() +
     facet_grid(reformulate(".",var)) +
     theme_few() +
     theme(legend.position = "bottom") +
-    geom_text(data = x %>% filter(CoreChurn != "Não Core"), aes(y=100 - Percentual, x=MesCriacao, label= Percentual)) +
+    geom_text(data = x %>% filter(ChurnAlvo != "Não"), aes(y= Percentual, x=MesCriacao, label= Percentual)) +
     labs(title = paste(c))
 }
 
